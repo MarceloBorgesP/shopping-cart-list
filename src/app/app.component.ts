3f2287db6c9ece0633d15e9dbe406b003e54e92f
Marcelo Borges <borgespereira.marcelo@gmail.com>
2019-03-12 17:10:44 -0300

    Add Header navigation to shopping list and recipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopping-cart-list';
+  loadedFeature = 'recipes';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
