1d7707be66da7b5c79e6590db2e97afb5efcd605
Marcelo Borges <borgespereira.marcelo@gmail.com>
2019-03-17 10:33:03 -0300

    Add Routes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,15 +9,21 @@ import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit() {
-  }
-
-  sendToSL() {
-    this.shoppingListService.addIngredients(this.recipe.ingredients);
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params.id
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    );
   }
 
 }
